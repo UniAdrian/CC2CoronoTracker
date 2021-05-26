99251181a437c663881fe8fbc6e16a562eae6908
uk077035 <uk077035@student.uni-kassel.de>
2021-05-26 19:06:47 +0200

    Initial commit Just an empty Activity.

---
+++ app/src/main/java/de/uni/cc2coronotracker/MainActivity.java
@@ -0,0 +1,14 @@
+package de.uni.cc2coronotracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
