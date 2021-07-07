1b00c180c37da056cee7c552f48275a11bfebf30
JRose <jisharose117@gmail.com>
2021-07-08 00:39:56 +0200

    QR Code scanner, Validation has to be done

--- app/src/main/java/de/uni/cc2coronotracker/ui/views/ReadQRFragment.java
+++ app/src/main/java/de/uni/cc2coronotracker/ui/views/ReadQRFragment.java
@@ -1,66 +1,102 @@
 package de.uni.cc2coronotracker.ui.views;
 
+import android.Manifest;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.Scanner;
 
 import de.uni.cc2coronotracker.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReadQRFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ReadQRFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ReadQRFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReadQR.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReadQRFragment newInstance(String param1, String param2) {
-        ReadQRFragment fragment = new ReadQRFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    CodeScanner codeScanner;
+    CodeScannerView scannerView;
+    TextView resultData;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_read_qr, container, false);
+        View view = inflater.inflate(R.layout.fragment_read_qr, container, false);
+        scannerView = view.findViewById(R.id.scannerView);
+        resultData = view.findViewById(R.id.resultsOfScan);
+        codeScanner = new CodeScanner(view.getContext(), scannerView);
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        Code for Validation need to be done instead the QR code value is displayed in TextView
+                        resultData.setText(result.getText());
+                    }
+                });
+
+                scannerView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        codeScanner.startPreview();
+
+                    }
+                });
+
+            }
+        });return view;
     }
-}
+
+    private void runOnUiThread(Runnable runnable) {
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        requestForCamera();
+    }
+
+    private void requestForCamera() {
+        Dexter.withContext(scannerView.getContext()).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(getActivity(),"Camera Permission is Required",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+
+            }
+        }).check();
+    }
+    }
