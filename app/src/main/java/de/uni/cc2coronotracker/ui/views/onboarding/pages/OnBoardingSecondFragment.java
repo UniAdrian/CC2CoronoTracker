0e9a5b605659294a5ff9debfcc69f0112d0156cc
mehedi332662 <mehedihasanjobbd@gmail.com>
2021-07-08 23:15:54 +0200

    Implementation done for on boarding feature

--- app/src/main/java/de/uni/cc2coronotracker/ui/views/onboarding/pages/OnBoardingSecondFragment.java
+++ app/src/main/java/de/uni/cc2coronotracker/ui/views/onboarding/pages/OnBoardingSecondFragment.java
@@ -3,6 +3,7 @@ package de.uni.cc2coronotracker.ui.views.onboarding.pages;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +14,19 @@ import de.uni.cc2coronotracker.R;
 public class OnBoardingSecondFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding_second, container, false);
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        View v = inflater.inflate(R.layout.fragment_on_boarding_second, container, false);
+        final ViewPager2 viewPager = getActivity() != null ? getActivity().findViewById(R.id.viewpager) : null;
+        v.findViewById(R.id.next_btn).setOnClickListener(v1 -> {
+            if(viewPager != null) {
+                viewPager.setCurrentItem(2);
+            }
+        });
+        return v;
     }
+
 }
--- app/src/main/java/de/uni/cc2coronotracker/ui/views/splash/SplashFragment.java
+++ app/src/main/java/de/uni/cc2coronotracker/ui/views/splash/SplashFragment.java
@@ -1,9 +1,15 @@
 package de.uni.cc2coronotracker.ui.views.splash;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +17,42 @@ import android.view.ViewGroup;
 import de.uni.cc2coronotracker.R;
 
 public class SplashFragment extends Fragment {
+
+    private static final String TAG = "SplashFragment";
+    private static final int SPLASH_SCREEN_SHOWING_TIME = 2000;
+    private static final String SP_TAG = "on_boarding_sp";
+    private static final String SP_KEY = "on_boarding_status";
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false);
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        View v = inflater.inflate(R.layout.fragment_splash, container, false);
+        navigateToDesiredPageAfterMentionedTime(v);
+        return v;
+    }
+
+    private void navigateToDesiredPageAfterMentionedTime(View view) {
+        new Handler(Looper.getMainLooper()).postDelayed(
+                () -> {
+                    boolean isAppLaunchedBefore = isAppLaunchedBefore();
+                    Log.d(TAG, "navigateToDesiredPageAfterMentionedTime: isAppLaunchedBefore? " + isAppLaunchedBefore);
+                    int action = isAppLaunchedBefore ? R.id.action_splashFragment_to_startScreen : R.id.action_splashFragment_to_onBoardingFragment;
+                    //int action = isAppLaunchedBefore ? R.id.action_splashFragment_to_onBoardingFragment : R.id.action_splashFragment_to_onBoardingFragment;
+                    Navigation.findNavController(view).navigate(action);
+                }, SPLASH_SCREEN_SHOWING_TIME
+        );
+    }
+
+    private boolean isAppLaunchedBefore() {
+        if(getActivity() != null) {
+            SharedPreferences sp = getActivity().getSharedPreferences(SP_TAG, Context.MODE_PRIVATE);
+            boolean retValue = sp.getBoolean(SP_KEY, false);
+            sp.edit().putBoolean(SP_KEY, true).apply();
+            return retValue;
+        }
+        return false;
     }
 }
