6edc764c8a3a3605819e410515659fda46cecc8e
mehedi332662 <mehedihasanjobbd@gmail.com>
2021-07-08 22:59:33 +0200

    Adding all the on boarding fragments

---
+++ app/src/main/java/de/uni/cc2coronotracker/ui/views/onboarding/OnBoardingFragment.java
@@ -0,0 +1,21 @@
+package de.uni.cc2coronotracker.ui.views.onboarding;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.uni.cc2coronotracker.R;
+
+
+public class OnBoardingFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_on_boarding, container, false);
+    }
+}
