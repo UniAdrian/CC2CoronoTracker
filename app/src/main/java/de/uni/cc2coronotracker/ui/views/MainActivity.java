24862d44611e28da44c1923924ff7fb852b46337
uk077035 <uk077035@student.uni-kassel.de>
2021-08-07 15:44:09 +0200

    Added some simple documentation to MainActivity.java

--- app/src/main/java/de/uni/cc2coronotracker/ui/views/MainActivity.java
+++ app/src/main/java/de/uni/cc2coronotracker/ui/views/MainActivity.java
@@ -1,5 +1,7 @@
 package de.uni.cc2coronotracker.ui.views;
 
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
@@ -27,7 +30,11 @@ import de.uni.cc2coronotracker.helper.ContextMediator;
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Identifies a single location availability request. Mainly used by the LocationProvider.
+     */
     public static final int LOCATION_AVAILABILITY_REQUEST = 1337;
+
 
     @Inject
     public ContextMediator ctxMediator;
@@ -54,12 +61,14 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
+        // Create and integrate our custom toolbar with appcompat and the navigation system.
         Toolbar toolbar = findViewById(R.id.app_toolbar_top);
         setSupportActionBar(toolbar);
 
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        // Sets the app to fullscreen+no-decorations for the onboarding and splash screens.
         navController.addOnDestinationChangedListener((controller, d, arguments) -> {
             boolean condition = d.getId() == R.id.splashFragment || d.getId() == R.id.onBoardingFragment;
             toolbar.setVisibility(condition ? View.GONE : View.VISIBLE);
@@ -82,8 +91,16 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
         });
+
+        // Use Day/Night mode according to the system settings.
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM);
     }
 
+
+    /**
+     * Called when the app returns from pause.
+     * Restarts the ctxMediator observer.
+     */
     @Override
     public void onResume() {
         super.onResume();
@@ -102,12 +119,24 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+
+    /**
+     * Inflates the actionbar default menu. Can be overwritten or extended by fragments as needed.
+     * @param menu The menu to be inflated to
+     * @return true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.actionbar_main_menu, menu);
         return true;
     }
 
+
+    /**
+     * Simple navigation to the preferences page using sage args via the action bar
+     * @param item Not used, default signature
+     * @return true - consumes the event
+     */
     public boolean gotoPreferences(MenuItem item) {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         if (navController.getCurrentDestination() != null && navController.getCurrentDestination().getId() != R.id.preferencesFragment) {
@@ -116,6 +145,13 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+    /**
+     * Receives certain activityResults and delegates them to the appropriate shared ViewModels
+     * @param requestCode The request code of the activity, used to identify the receiver of the result
+     * @param resultCode Activity Result, Usually SUCCESS or ABORTED
+     * @param data The actual intent data passed to us from the sending activity.
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
